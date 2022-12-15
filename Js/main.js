8aed3afa0c8da12d85bfd2b03719acb2c6e8bfd1
Hafiz Bashirov <101920150+hafizbashirov@users.noreply.github.com>
2022-12-15 18:32:42 +0400

    Add files via upload

---
+++ Js/main.js
@@ -0,0 +1,48 @@
+function changeStyle(){    
+    
+     var width = document.getElementById('widthx').value
+     var height = document.getElementById('heightx').value
+     var bgColor = document.getElementById('bgColorx').value
+     var position = document.getElementById('positionx').value
+     var top = document.getElementById('topx').value
+     var left = document.getElementById('leftx').value
+     var right = document.getElementById('rightx').value
+     var bottom = document.getElementById('bottomx').value
+     var tranx = document.getElementById('transitionx').value
+     var borx = document.getElementById('borderx').value
+     var borcolx = document.getElementById('bordercolorx').value
+     
+     
+     
+     document.getElementById('square').style.width = width
+     document.getElementById('square').style.height = height
+     document.getElementById('square').style.background = bgColor
+     document.getElementById('square').style.position = position
+     document.getElementById('square').style.top= top 
+     document.getElementById('square').style.left = left
+     document.getElementById('square').style.right = right
+     document.getElementById('square').style.bottom= bottom 
+     document.getElementById('square').style.transition = tranx
+     document.getElementById('square').style.borderWidth = borx
+     document.getElementById('square').style.borderColor = borcolx
+     
+     
+     
+    
+    
+      
+    
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
